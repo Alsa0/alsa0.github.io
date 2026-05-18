5d552ed8a7cc28a7041ca9868a76ad7e7e89fc78
Solondrainy Aurélien ANTSARILALA <NH445@ivandry.net>
2026-05-18 10:43:45 +0300

    Projets NextHope clients ajoutes

--- js/projects.js
+++ js/projects.js
@@ -339,9 +339,9 @@ const projects = [
 
 const categories = [
   { key: "all", label: "🔥 Tous" },
-  { key: "reseaux", label: "<i class='fa-solid fa-network-wired'></i> Réseaux" },
-  { key: "cloud", label: "<i class='fa-solid fa-cloud'></i> Cloud" },
-  { key: "securite", label: "<i class='fa-solid fa-lock'></i> Sécurité" },
-  { key: "voip", label: "<i class='fa-solid fa-phone'></i> VoIP" },
-  { key: "dev", label: "<i class='fa-solid fa-code'></i> Dev" },
+  { key: "reseaux", label: "<i class='fa-solid fa-network-wired' style='color:#38bdf8'></i> Réseaux" },
+  { key: "cloud", label: "<i class='fa-solid fa-cloud' style='color:#818cf8'></i> Cloud" },
+  { key: "securite", label: "<i class='fa-solid fa-lock' style='color:#f97316'></i> Sécurité" },
+  { key: "voip", label: "<i class='fa-solid fa-phone' style='color:#22c55e'></i> VoIP" },
+  { key: "dev", label: "<i class='fa-solid fa-code' style='color:#00ffb3'></i> Dev" },
 ];
